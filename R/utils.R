05db126d7c3d0b2b02bf39f52f083e8ba33b1dfa
mps9506 <mpschramm@gmail.com>
2018-07-20 12:46:05 -0500

    :hankey: Utility Functions
    list_NA creates a tibble with NA values for specified character vector.

---
+++ R/utils.R
@@ -0,0 +1,12 @@
+
+# function to return tibble with NAs as specified
+# list_NA(c("mean", "lwr.ci", "upr.ci"))
+list_NA <- function(...) {
+  names <- c(...)
+  results <- as.list(c(rep(NA, length(names))))
+  names(results) <- names
+  results <- tibble::as_tibble(results)
+  results[1:length(results)] <- as.numeric(results[1:length(results)])
+  return(results)
+}
+
