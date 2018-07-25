f107afd0d82c5593c4c44cce740b78a9d1195311
mps9506 <mpschramm@gmail.com>
2018-07-25 07:51:04 -0500

    Ungroup sample dataset

--- data-raw/Dissolved_Oxygen.R
+++ data-raw/Dissolved_Oxygen.R
@@ -1,13 +1,14 @@
 library(dplyr)
 
-Dissolved_Oxygen <- readr::read_delim(file = here::here("data/DO.txt"), delim = "|" ) %>%
+Dissolved_Oxygen <- readr::read_delim(file = here::here("data-raw/DO.txt"), delim = "|" ) %>%
   mutate(`End Date` = as.Date(`End Date`, "%m/%d/%Y")) %>%
   group_by(`Station ID`, `End Date`, `Parameter Code`, `Parameter Description`) %>%
   summarise(Average_DO = mean(Value),
             Min_DO = min(Value)) %>%
+  ungroup() %>%
   rename(Station_ID = `Station ID`,
          Date = `End Date`,
          Param_Code = `Parameter Code`,
          Param_Desc = `Parameter Description`)
 
-devtools::use_data(Dissolved_Oxygen)
+devtools::use_data(Dissolved_Oxygen, overwrite = TRUE)
